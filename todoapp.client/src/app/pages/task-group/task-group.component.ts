1cf1f7f908ff4133ec40f94bd845d7b412ab06da
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-09 19:54:05 +0530

    Added the task group component

---
+++ todoapp.client/src/app/pages/task-group/task-group.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-group',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-group.component.html',
+  styleUrl: './task-group.component.scss'
+})
+export class TaskGroupComponent {
+
+}
