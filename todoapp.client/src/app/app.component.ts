910c83e5d751ded72c5b6e450fd23175b9f57a57
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-09 14:35:48 +0530

    Added layout to app

--- todoapp.client/src/app/app.component.ts
+++ todoapp.client/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { AuthCardComponent } from './components/auth-card/auth-card.component';
 import { AuthenticationComponent } from './pages/authentication/authentication.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { SideBarComponent } from './layouts/side-bar/side-bar.component';
+import { MainContentComponent } from './layouts/main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,SideBarComponent],
+  imports: [CommonModule, RouterOutlet,SideBarComponent,MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
