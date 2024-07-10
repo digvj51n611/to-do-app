4416b3872bde5b9c1479522259df7f3a60aabe68
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-10 11:10:28 +0530

    Added the task item

--- todoapp.client/src/app/components/button-icon/button-icon.component.ts
+++ todoapp.client/src/app/components/button-icon/button-icon.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component , Input } from '@angular/core';
 
 @Component({
   selector: 'app-button-icon',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button-icon.component.html',
   styleUrl: './button-icon.component.scss'
 })
 export class ButtonIconComponent {
-
+  @Input({required : true}) buttonName! : string;
+  @Input() bgClassList : string = '';
+  @Input() btnClassList : string = '';
 }
