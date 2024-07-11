8329216d4998eac322f4a5aac750eaaa09b4ba8c
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-11 14:06:45 +0530

    added the child components to the dashboard

--- todoapp.client/src/app/pages/dashboard/dashboard.component.ts
+++ todoapp.client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { TaskGroupComponent } from '../task-group/task-group.component';
+import { BannerComponent } from '../../components/banner/banner.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [TaskGroupComponent],
+  imports: [TaskGroupComponent,BannerComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-  
+
 }
