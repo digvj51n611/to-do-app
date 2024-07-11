5546e25dc906ace3a06c907235de0076395f0e37
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-11 11:18:47 +0530

    added the task group component to the dashboard

--- todoapp.client/src/app/pages/dashboard/dashboard.component.ts
+++ todoapp.client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { TaskGroupComponent } from '../task-group/task-group.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [TaskGroupComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  
 }
