78e941919458faa53655569470b3a5a89aafb3c0
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-11 11:15:53 +0530

    Added the taskgroup compoent

--- todoapp.client/src/app/layouts/main-content/main-content.component.ts
+++ todoapp.client/src/app/layouts/main-content/main-content.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { RouterOutlet } from '@angular/router';
+import { TaskGroupComponent } from '../../pages/task-group/task-group.component';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,RouterOutlet,DashboardComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
