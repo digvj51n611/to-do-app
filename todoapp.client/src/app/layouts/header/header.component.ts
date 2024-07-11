d081280f19b1dd4e657d5274d1f6b00c8f14108b
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-11 11:16:32 +0530

    Added the header

--- todoapp.client/src/app/layouts/header/header.component.ts
+++ todoapp.client/src/app/layouts/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonIconComponent } from '../../components/button-icon/button-icon.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ButtonIconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  heading : string = "Dashboard";
 }
