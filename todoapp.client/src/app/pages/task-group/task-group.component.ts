0d8fc1d371807f747b3d75d6c77e60019919c0b5
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-11 14:05:42 +0530

    Added the current date funtionality

--- todoapp.client/src/app/pages/task-group/task-group.component.ts
+++ todoapp.client/src/app/pages/task-group/task-group.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { TaskItemComponent } from '../../components/task-item/task-item.component';
 import { StatsCardComponent } from '../../components/stats-card/stats-card.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-group',
   standalone: true,
-  imports: [TaskItemComponent,StatsCardComponent],
+  imports: [TaskItemComponent,StatsCardComponent,],
   templateUrl: './task-group.component.html',
-  styleUrl: './task-group.component.scss'
+  styleUrl: './task-group.component.scss',
+  providers :[DatePipe]
 })
 export class TaskGroupComponent {
   taskGroupHeader : string = "Today's Tasks";
   tempBoolean : boolean = false;
   tempNumber : number = 0.4;
+  formattedDate : string | null ;
+  /**
+   *
+   */
+  constructor(private datePipe : DatePipe) {
+    const date = new Date(); // Month is zero-based, so 11 is December
+    this.formattedDate = this.datePipe.transform(date, 'EEEE, dd MMMM yyyy');    
+  }
 }
