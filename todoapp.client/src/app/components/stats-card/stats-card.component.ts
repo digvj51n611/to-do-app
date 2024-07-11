889cf92d6642f2e908f1da4af1839a3d4291b7e6
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-11 11:13:39 +0530

    Added the stats card ccmponent

--- todoapp.client/src/app/components/stats-card/stats-card.component.ts
+++ todoapp.client/src/app/components/stats-card/stats-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { NgClass, NgIf, PercentPipe } from '@angular/common';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-stats-card',
   standalone: true,
-  imports: [],
+  imports: [NgClass,PercentPipe,NgIf],
   templateUrl: './stats-card.component.html',
   styleUrl: './stats-card.component.scss'
 })
 export class StatsCardComponent {
-
+  @Input({required : true}) isActiveCard : boolean = false;
+  @Input({required : true}) ratioValue : number = 0;
 }
