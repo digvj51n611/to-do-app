4a19e97bca4e2ba4d1ca535554a2f4b7ed4e173a
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-11 11:07:03 +0530

    dashboard page added

--- todoapp.client/src/app/pages/task-group/task-group.component.ts
+++ todoapp.client/src/app/pages/task-group/task-group.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { TaskItemComponent } from '../../components/task-item/task-item.component';
+import { StatsCardComponent } from '../../components/stats-card/stats-card.component';
 
 @Component({
   selector: 'app-task-group',
   standalone: true,
-  imports: [],
+  imports: [TaskItemComponent,StatsCardComponent],
   templateUrl: './task-group.component.html',
   styleUrl: './task-group.component.scss'
 })
 export class TaskGroupComponent {
-
+  taskGroupHeader : string = "Today's Tasks";
+  tempBoolean : boolean = false;
+  tempNumber : number = 0.4;
 }
