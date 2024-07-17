df4e648f0280c83e35b74b91b0a7271fbb114d09
dighvijay-s-technovert <dighvijay.s@technovert.com>
2024-07-17 13:01:28 +0530

    added static functions

--- ToDoApp.data/Models/DataResponse.cs
+++ ToDoApp.data/Models/DataResponse.cs
@@ -1,16 +1,54 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ToDoApp.Data.Models
 {
-    public class DataResponse<T>
+    public class DataResponse<TData>
     {
         public bool IsSuccess { get; set; }
         public string Message { get; set; }
-        public T Data { get; set; }
+        public TData? Data { get; set; }
         public ErrorType ErrorType { get; set; }
+        public static DataResponse<TData> SuccessResult(TData data, string message = "fetch successful")
+        {
+            return new DataResponse<TData>
+            {
+                IsSuccess = true,
+                Data = data,
+                ErrorType = ErrorType.None,
+                Message = message
+            };
+        }
+        public static DataResponse<TData> FailureResult(ErrorType type, string message)
+        {
+            return new DataResponse<TData>
+            {
+                ErrorType = type,
+                Message = message
+            };
+        }
+        public static DataResponse<TData> FailureResult(Exception ex)
+        {
+            var errorType = GetErrorType(ex);
+            return FailureResult(errorType, ex.Message);
+        }
+
+        private static ErrorType GetErrorType(Exception ex)
+        {
+            return ex switch
+            {
+                SqlException => ErrorType.ConnectionError,
+                KeyNotFoundException => ErrorType.NotFoundError,
+                DbUpdateException => ErrorType.SourceError,
+                DbException => ErrorType.SourceError,
+                _ => ErrorType.UnknownError
+            };
+        }
     }
 }
